fdfe57294669536e26cb54656fd6758a8031d748
Michael Ludwig <michael.ludwig1392@gmail.com>
2026-01-06 00:41:51 +0100

    optimize teacher table calculation

--- src/teacher_table_generator.rs
+++ src/teacher_table_generator.rs
@@ -7,96 +7,47 @@ pub async fn get_all_timetables(
     week: Week,
 ) -> Result<(HashMap<Entity, WeekTimeTable>, Option<i32>), String> {
     let (classes, pre_selected) = get_classes(Week::current()).await?;
-    let res = crate::untis_client::get_multiple_timetables(week.clone(), &classes).await?;
-    let mut all_timetables: HashMap<Entity, WeekTimeTable> = HashMap::new();
-    for (class, table) in res.iter() {
-        all_timetables.insert(Entity::Class(class.clone()), table.clone());
+    let class_results = crate::untis_client::get_multiple_timetables(week.clone(), &classes).await?;
+
+    let mut all_timetables: HashMap<Entity, WeekTimeTable> = class_results
+        .into_iter()
+        .map(|(class, table)| (Entity::Class(class), table))
+        .collect();
+
+    let mut entity_lesson_map: HashMap<Entity, Vec<(usize, LessonBlock)>> = HashMap::new();
+
+    for table in all_timetables.values() {
+        for (i, day_table) in table.days.iter().enumerate() {
+            for lesson in &day_table.lessons {
+                for entity_wrapper in &lesson.entities {
+                    if entity_wrapper.status == ChangeStatus::Removed {
+                        continue;
+                    }
+
+                    match &entity_wrapper.data {
+                        Entity::Teacher(_) | Entity::Room(_) => {
+                            entity_lesson_map
+                                .entry(entity_wrapper.data.clone())
+                                .or_default()
+                                .push((i, lesson.clone()));
+                        }
+                        _ => {}
+                    }
+                }
+            }
+        }
     }
-    let class_timetables: Vec<WeekTimeTable> = res.into_values().collect();
-    let lessons = class_timetables
-        .iter()
-        .flat_map(|timetable| timetable.days.clone())
-        .flat_map(|day| day.lessons)
-        .collect::<Vec<_>>();
 
-    let teachers = lessons
-        .iter()
-        .flat_map(|lesson| &lesson.entities)
-        .filter(|e| e.status != ChangeStatus::Removed)
-        .filter_map(|e| {
-            if let Entity::Teacher(t) = e.data.clone() {
-                Some(t)
-            } else {
-                None
-            }
-        })
-        .collect::<Vec<_>>();
-
-    let rooms = lessons
-        .iter()
-        .flat_map(|lesson| &lesson.entities)
-        .filter(|e| e.status != ChangeStatus::Removed)
-        .filter_map(|e| {
-            if let Entity::Room(r) = e.data.clone() {
-                Some(r)
-            } else {
-                None
-            }
-        })
-        .collect::<Vec<_>>();
-
-    for t in teachers {
-        let mut table = WeekTimeTable {
+    for (entity, lessons) in entity_lesson_map {
+        let mut new_table = WeekTimeTable {
             days: vec![DayTimeTable { lessons: vec![] }; 5],
         };
 
-        for tt in &class_timetables {
-            for (i, dt) in tt.days.iter().enumerate() {
-                table.days[i].lessons.extend(
-                    dt.lessons
-                        .iter()
-                        .filter(|l| {
-                            l.entities.iter().any(|e| {
-                                if let Entity::Teacher(x) = e.data.clone() {
-                                    x == t
-                                } else {
-                                    false
-                                }
-                            })
-                        })
-                        .cloned(),
-                );
-            }
+        for (i, lesson) in lessons {
+            new_table.days[i].lessons.push(lesson);
         }
 
-        all_timetables.insert(Entity::Teacher(t), table);
-    }
-
-    for r in rooms {
-        let mut table = WeekTimeTable {
-            days: vec![DayTimeTable { lessons: vec![] }; 5],
-        };
-
-        for tt in &class_timetables {
-            for (i, dt) in tt.days.iter().enumerate() {
-                table.days[i].lessons.extend(
-                    dt.lessons
-                        .iter()
-                        .filter(|l| {
-                            l.entities.iter().any(|e| {
-                                if let Entity::Room(x) = e.data.clone() {
-                                    x == r
-                                } else {
-                                    false
-                                }
-                            })
-                        })
-                        .cloned(),
-                );
-            }
-        }
-
-        all_timetables.insert(Entity::Room(r), table);
+        all_timetables.insert(entity, new_table);
     }
 
     Ok((all_timetables, pre_selected))
